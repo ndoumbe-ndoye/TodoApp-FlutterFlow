fb68ce0a1d599c727dd439e866be908284143cb3
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-03-03 14:29:25 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/index.dart
@@ -0,0 +1,6 @@
+// Export pages
+export '/login/login_widget.dart' show LoginWidget;
+export '/task/task_widget.dart' show TaskWidget;
+export '/on_boarding/on_boarding_widget.dart' show OnBoardingWidget;
+export '/details/details_widget.dart' show DetailsWidget;
+export '/completed/completed_widget.dart' show CompletedWidget;
---
+++ lib/login/login_model.dart
@@ -0,0 +1,108 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import '/index.dart';
+import 'login_widget.dart' show LoginWidget;
+import 'package:flutter/material.dart';
+
+class LoginModel extends FlutterFlowModel<LoginWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final formKey = GlobalKey<FormState>();
+  // State field(s) for TabBar widget.
+  TabController? tabBarController;
+  int get tabBarCurrentIndex =>
+      tabBarController != null ? tabBarController!.index : 0;
+
+  // State field(s) for SignupEmail widget.
+  FocusNode? signupEmailFocusNode;
+  TextEditingController? signupEmailTextController;
+  String? Function(BuildContext, String?)? signupEmailTextControllerValidator;
+  String? _signupEmailTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Email address... is required';
+    }
+
+    if (!RegExp(kTextValidatorEmailRegex).hasMatch(val)) {
+      return 'Has to be a valid email address.';
+    }
+    return null;
+  }
+
+  // State field(s) for SignuPassword widget.
+  FocusNode? signuPasswordFocusNode;
+  TextEditingController? signuPasswordTextController;
+  late bool signuPasswordVisibility;
+  String? Function(BuildContext, String?)? signuPasswordTextControllerValidator;
+  String? _signuPasswordTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Password is required';
+    }
+
+    if (val.length < 7) {
+      return 'Requires at least 7 characters.';
+    }
+
+    return null;
+  }
+
+  // State field(s) for ConfirmPassword widget.
+  FocusNode? confirmPasswordFocusNode;
+  TextEditingController? confirmPasswordTextController;
+  late bool confirmPasswordVisibility;
+  String? Function(BuildContext, String?)?
+      confirmPasswordTextControllerValidator;
+  String? _confirmPasswordTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Confirm password... is required';
+    }
+
+    if (val.length < 7) {
+      return 'Requires at least 7 characters.';
+    }
+
+    return null;
+  }
+
+  // State field(s) for LoginEmail widget.
+  FocusNode? loginEmailFocusNode;
+  TextEditingController? loginEmailTextController;
+  String? Function(BuildContext, String?)? loginEmailTextControllerValidator;
+  // State field(s) for LoginPassword widget.
+  FocusNode? loginPasswordFocusNode;
+  TextEditingController? loginPasswordTextController;
+  late bool loginPasswordVisibility;
+  String? Function(BuildContext, String?)? loginPasswordTextControllerValidator;
+
+  @override
+  void initState(BuildContext context) {
+    signupEmailTextControllerValidator = _signupEmailTextControllerValidator;
+    signuPasswordVisibility = false;
+    signuPasswordTextControllerValidator =
+        _signuPasswordTextControllerValidator;
+    confirmPasswordVisibility = false;
+    confirmPasswordTextControllerValidator =
+        _confirmPasswordTextControllerValidator;
+    loginPasswordVisibility = false;
+  }
+
+  @override
+  void dispose() {
+    tabBarController?.dispose();
+    signupEmailFocusNode?.dispose();
+    signupEmailTextController?.dispose();
+
+    signuPasswordFocusNode?.dispose();
+    signuPasswordTextController?.dispose();
+
+    confirmPasswordFocusNode?.dispose();
+    confirmPasswordTextController?.dispose();
+
+    loginEmailFocusNode?.dispose();
+    loginEmailTextController?.dispose();
+
+    loginPasswordFocusNode?.dispose();
+    loginPasswordTextController?.dispose();
+  }
+}
